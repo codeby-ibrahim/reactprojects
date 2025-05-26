5f830f17589e4564f6911f5199876d962e5df09b
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com.com>
2025-05-26 12:07:04 +0500

    commitfirst

---
+++ src/commpnet/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div style={{
+      width: '300px',
+      height: '200px',
+      backgroundColor: '#f0f0f0',
+      border: '1px solid #ccc',
+      borderRadius: '8px',
+      display: 'flex',
+    }}>
+          
+    </div>
+  )
+}
+
+export default Card
